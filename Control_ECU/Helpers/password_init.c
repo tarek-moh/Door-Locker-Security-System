1a54c2c63605fe575b19cdf56ac02220ce05f0ba
Nour Mamdouh <nourelbakry06@gmail.com>
2025-12-21 03:10:03 +0200

    added missing eeprom init

--- Control_ECU/Helpers/password_init.c
+++ Control_ECU/Helpers/password_init.c
@@ -2,10 +2,12 @@
 
 bool is_password_init(void)
 {
+    EEPROM_HW_Init();
     return ((EEPROM_HW_ReadWord(1, 2) & 1) == 1);
 }
 
 void set_init_flag(void)
 {
+    EEPROM_HW_Init();
     EEPROM_HW_WriteWord(1, 2, 1);
 }
